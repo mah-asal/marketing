876e7c83fec44eec1317afea5c9371395f1ee590
Mirza <sayheymirza@gmail.com>
2024-05-03 15:33:12 +0330

    [added] number badges

--- src/app/routes/home.component.ts
+++ src/app/routes/home.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { PhoneComponent } from '../components/phone.component';
 import { AppService } from '../services/app.service';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +16,7 @@ import { MatIconModule } from '@angular/material/icon';
         <app-phone />
       </div>
 
-      <div class="flex flex-col items-center md:items-start gap-2 px-4 py-10">
+      <div class="flex flex-col items-center md:items-start -md:text-center gap-2 px-4 py-10">
         <h3 class="font-extrabold text-3xl">
           شبکه اجتماعی من
         </h3>
@@ -33,10 +34,10 @@ import { MatIconModule } from '@angular/material/icon';
         <div class="flex flex-nowrap items-center gap-2">
           <img ngSrc="/assets/images/qrcode.png" alt="Mahasal qr code" width="150" height="150" class="p-2 bg-white rounded-xl border hidden md:block" />
 
-          <div class="flex flex-col gap-2">
-            <div class="flex flex-wrap gap-2">
+          <div class="flex flex-col -md:items-center gap-2">
+            <div class="flex -md:flex-col md:flex-wrap -md:items-center gap-2">
               <a href="/assets/mahasal.apk" download="mahasal.apk" class="flex flex-nowrap items-center gap-6 w-[222px] bg-white rounded-lg px-4 py-2 border transition-all active:scale-95">
-                <div class="flex flex-col flex-1">
+                <div class="flex flex-col text-start flex-1">
                   <span class="text-sm text-gray-600">دانلود</span>
                   <strong class="font-bold text-lg">مستقیم</strong>
                 </div>
@@ -54,7 +55,7 @@ import { MatIconModule } from '@angular/material/icon';
               </a> -->
             </div>
 
-            <div class="flex flex-wrap gap-2">
+            <div class="flex -md:flex-col md:flex-wrap -md:items-center gap-2">
               <a class="transition-all active:scale-95">
                 <img src="/assets/images/cafebaazar-badge.png" alt="Cafebaazar download badge" class="h-[66px]" />
               </a>
@@ -68,15 +69,41 @@ import { MatIconModule } from '@angular/material/icon';
 
         <div class="w-full h-[2px] my-4 bg-gradient-to-l from-gray-200 to-transparent"></div>
 
-        <div class="flex flex-nowrap items-center gap-10">
+        <div class="flex flex-nowrap items-center gap-10 text-start">
           <img ngSrc="/assets/images/confetti.png" alt="confetti icon" width="46" height="46" />
 
           <h3 class="font-bold text-xl">کسب درآمد میلیونی با دعوت از دوستان</h3>
         </div>
       </div>
     </section>
+    
+    <section class="grid grid-cols-2 md:grid-cols-4 container mx-auto my-10 p-4">
+      <div class="flex flex-col items-center justify-center gap-4">
+        <span class="text-gray-500">تعداد کاربران ما</span>
 
-    <section class="grid grid-cols-1 md:grid-cols-2 container mx-auto my-10">
+        <strong class="text-4xl">{{count}} نفر</strong>
+      </div>
+
+      <div class="flex flex-col items-center justify-center gap-4">
+        <span class="text-gray-500">تعداد کاربران فعال ما</span>
+
+        <strong class="text-4xl">+۱۰۰۰ نفر</strong>
+      </div>
+
+      <div class="flex flex-col items-center justify-center gap-4">
+        <span class="text-gray-500">سایقه فعالیت ما</span>
+
+        <strong class="text-4xl">۳ سال</strong>
+      </div>
+
+      <div class="flex flex-col items-center justify-center gap-4">
+        <span class="text-gray-500">تعداد پشتیبان های ما</span>
+
+        <strong class="text-4xl">۳۲ نفر</strong>
+      </div>
+    </section>
+
+    <section class="grid grid-cols-1 md:grid-cols-2 container mx-auto my-10 p-4">
       <div class="flex flex-col gap-2">
         <div class="flex flex-nowrap items-center gap-4">
           <svg width="36" height="36" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.4255 1.83301C6.36285 1.83301 2.25879 5.93706 2.25879 10.9997C2.25879 16.0623 6.36285 20.1663 11.4255 20.1663C16.4881 20.1663 20.5921 16.0623 20.5921 10.9997C20.5921 5.93706 16.4881 1.83301 11.4255 1.83301ZM10.7173 7.85462L8.87012 9.32557C8.09516 9.9427 7.70768 10.2513 7.78331 10.5958L7.78733 10.6129C7.8735 10.9553 8.36467 11.0869 9.347 11.3501C9.89288 11.4963 10.1658 11.5695 10.2939 11.7764L10.3005 11.7873C10.424 11.9967 10.3537 12.2589 10.2132 12.7834L10.1764 12.9204C9.78676 14.3746 9.59193 15.1017 9.96715 15.3365C10.3424 15.5713 10.9394 15.0959 12.1334 14.1451L13.9805 12.6742C14.7555 12.057 15.1429 11.7485 15.0673 11.404L15.0633 11.3869C14.9771 11.0445 14.486 10.9129 13.5036 10.6497C12.9577 10.5034 12.6848 10.4303 12.5568 10.2234L12.5502 10.2125C12.4267 10.0031 12.4969 9.74085 12.6375 9.21636L12.6742 9.07925C13.0638 7.6251 13.2587 6.89802 12.8834 6.66321C12.5082 6.4284 11.9112 6.90381 10.7173 7.85462Z" fill="#FF3C5E"></path></svg>
@@ -87,7 +114,7 @@ import { MatIconModule } from '@angular/material/icon';
       </div>
     </section>
 
-    <section class="grid grid-cols-1 md:grid-cols-2 container mx-auto my-10">
+    <section class="grid grid-cols-1 md:grid-cols-2 container mx-auto my-10 p-4">
       <div></div>
       <div class="flex flex-col gap-2">
         <div class="flex flex-nowrap items-center gap-4">
@@ -99,7 +126,7 @@ import { MatIconModule } from '@angular/material/icon';
       </div>
     </section>
 
-    <section class="grid grid-cols-1 md:grid-cols-2 container mx-auto my-10">
+    <section class="grid grid-cols-1 md:grid-cols-2 container mx-auto my-10 p-4">
       <div class="flex flex-col gap-2">
         <div class="flex flex-nowrap items-center gap-4">
           <svg width="36" height="36" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path _ngcontent-ng-c2715060265="" d="M15.5837 10.9997C18.115 10.9997 20.167 8.94765 20.167 6.41634C20.167 3.88504 18.115 1.83301 15.5837 1.83301C13.0524 1.83301 11.0003 3.88504 11.0003 6.41634C11.0003 7.14953 11.1725 7.84251 11.4786 8.45706C11.5599 8.62037 11.587 8.80704 11.5398 8.98329L11.2669 10.0036C11.1483 10.4465 11.5535 10.8517 11.9964 10.7331L13.0167 10.4602C13.193 10.413 13.3796 10.4401 13.5429 10.5214C14.1575 10.8275 14.8505 10.9997 15.5837 10.9997Z" fill="#995BFF"></path><path _ngcontent-ng-c2715060265="" d="M7.36747 6.70649L7.96239 7.77251C8.49929 8.73453 8.28376 9.99655 7.43816 10.8421C7.43816 10.8422 7.43816 10.8421 7.43816 10.8422C7.43804 10.8423 6.41258 11.868 8.27215 13.7275C10.131 15.5864 11.1567 14.5624 11.1575 14.5615C11.1575 14.5615 11.1575 14.5615 11.1576 14.5615C12.0032 13.7159 13.2652 13.5004 14.2272 14.0373L15.2932 14.6322C16.7459 15.4429 16.9174 17.4801 15.6405 18.757C14.8733 19.5243 13.9334 20.1213 12.8943 20.1607C11.1452 20.227 8.17476 19.7843 5.19507 16.8046C2.21537 13.8249 1.7727 10.8545 1.83901 9.10533C1.8784 8.0663 2.47541 7.12638 3.24266 6.35913C4.51953 5.08226 6.55675 5.25382 7.36747 6.70649Z" fill="#995BFF"></path></svg>
@@ -115,5 +142,20 @@ import { MatIconModule } from '@angular/material/icon';
   },
 })
 export class HomeComponent {
-  constructor(public appService: AppService) { }
+  public count: string = '0';
+
+  constructor(public appService: AppService, private httpService: HttpService) { }
+
+  ngOnInit() {
+    this.httpService.request({
+      method: 'GET',
+      path: '/api/v1/call/api.v1.profile.search?limit=1'
+    }).subscribe({
+      next: (res) => {
+        if(res['status']) {
+          this.count = new Number( res['meta']['total']).toLocaleString('fa-IR');
+        }
+      }
+    })
+  }
 }
